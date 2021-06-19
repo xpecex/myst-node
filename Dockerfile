65ad64b764800c95ae4c3a9f7e6f5c6ac77cba57
Pedro Filipe <xpecex@outlook.com>
2021-06-19 15:15:51 -0300

    improve the build process

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,19 @@
 FROM ubuntu:18.04
 
+# BUILD ARGs
+ARG TARGETPLATFORM
+ARG IMAGE_NAME
+ARG IMAGE_VER
+ARG IMAGE_AUTHOR
+ARG IMAGE_VENDOR
+ARG IMAGE_REF
+ARG IMAGE_DESC
+ARG IMAGE_BUILD_DATE
+ARG IMAGE_URL
+ARG IMAGE_LICENSE
+
 # Set Debian Frontend
 ENV DEBIAN_FRONTEND=noninteractive
-
-# BUILD ARGs
-ARG BUILD_DATE
-ARG VCS_REF
-ARG VERSION
 
 # MYSTERIUM NETWORK ENV
 ENV OS_DIR_CONFIG="/etc/mysterium-node"
@@ -14,44 +21,36 @@ ENV OS_DIR_DATA="/var/lib/mysterium-node"
 ENV OS_DIR_RUN="/var/run/mysterium-node"
 
 # Labels
-LABEL org.label-schema.build-date=$BUILD_DATE \
-    org.label-schema.description="Mysterium Network Node docker image multiarch (unofficial)" \
-    org.label-schema.name="xpecex/myst-node" \
-    org.label-schema.schema-version="1.0" \
-    org.label-schema.usage="https://github.com/xpecex/myst-node" \
-    org.label-schema.vcs-url="https://github.com/xpecex/myst-node" \
-    org.label-schema.vcs-ref=$VCS_REF \
-    org.label-schema.vendor="xPeCex" \
-    org.label-schema.version=$VERSION
+LABEL org.opencontainers.image.title="$IMAGE_NAME" \
+    org.opencontainers.image.authors="$IMAGE_AUTHOR" \
+    org.opencontainers.image.vendor="$IMAGE_VENDOR" \
+    org.opencontainers.image.description="$IMAGE_DESC" \
+    org.opencontainers.image.created="$IMAGE_BUILD_DATE" \
+    org.opencontainers.image.url="$IMAGE_URL" \
+    org.opencontainers.image.source="$IMAGE_URL" \
+    org.opencontainers.image.documentation="$IMAGE_URL" \
+    org.opencontainers.image.revision="$IMAGE_REF" \
+    org.opencontainers.image.version="$IMAGE_VER" \
+    org.opencontainers.image.licenses="$IMAGE_LICENSE"
 
 # Install Dependencies
 RUN apt update && \
-    apt upgrade -yqq && \
     echo "resolvconf resolvconf/linkify-resolvconf boolean false" | debconf-set-selections && \
-    apt install -yqq wget openvpn wireguard resolvconf iptables iproute2 sudo wireguard libcap2-bin && \
+    apt install --no-install-recommends -yqq wget openvpn wireguard resolvconf iptables iproute2 sudo wireguard libcap2-bin && \
     rm -rf /var/cache/apt/* /var/lib/apt/lists/*
 
-# Copy Download file
-COPY download.sh /tmp/download.sh
+# COPY Entrypoint and prepare-env scripts
+COPY $TARGETPLATFORM/myst_node.deb /tmp/myst_node.deb
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+COPY prepare-run-env.sh /usr/local/bin/prepare-run-env.sh
 
 # Install Mysterium-Node
-RUN chmod +x /tmp/download.sh && \
-    ./tmp/download.sh && \
-    dpkg -i /tmp/myst_linux_$VERSION.deb && \
-    apt install -f -yqq && \
+RUN dpkg -i /tmp/myst_node.deb && \
     mkdir -p $OS_DIR_CONFIG && \
     mkdir -p $OS_DIR_DATA && \
     mkdir -p $OS_DIR_RUN && \
     ln -s /lib/ld-linux-*.so.3 /lib/ld-linux.so.3 && \
     rm -rf /tmp/*
-
-# Download Entrypoint and prepare-env scripts
-ADD https://raw.githubusercontent.com/mysteriumnetwork/node/master/bin/docker/docker-entrypoint.sh /docker-entrypoint.sh
-ADD https://raw.githubusercontent.com/mysteriumnetwork/node/master/bin/helpers/prepare-run-env.sh /usr/local/bin/prepare-run-env.sh
-
-# Change permissions
-RUN chmod +x /docker-entrypoint.sh && \
-    chmod +x /usr/local/bin/prepare-run-env.sh
 
 # Set default entrypoint
 ENTRYPOINT [ "/docker-entrypoint.sh" ]
--- build.sh
+++ build.sh
@@ -1,60 +1,135 @@
 #!/bin/bash
 
-# RELEASE LIST
+# IMAGE VARIABLES
+IMAGE_NAME="xpecex/myst-node"
+IMAGE_VER=""
+IMAGE_AUTHOR="xPeCex <xpecex@outlook.com>"
+IMAGE_VENDOR=$IMAGE_AUTHOR
+IMAGE_REF="$(git rev-parse --short HEAD)"
+IMAGE_DESC="Mysterium Network Node docker image multiarch (unofficial)"
+IMAGE_BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
+IMAGE_URL="https://github.com/xpecex/myst-node"
+IMAGE_LICENSE="GPL-3.0+"
+IMAGE_ALT_REF="$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 7 | head -n 1)"
+
+# RELEASES LIST
 RELEASES=(
     "0.47.2"
+    "0.47.1"
+    "0.47.0"
+    "0.46.1"
 )
-
-# GET LATEST
-LATEST=$(curl -s "https://github.com/mysteriumnetwork/node/releases/latest" | cut -d'/' -f 8 | cut -d'"' -f 1)
+# LATEST RELEASE
+LATEST_RELEASE=$(curl -s "https://github.com/mysteriumnetwork/node/releases/latest" | cut -d'/' -f 8 | cut -d'"' -f 1)
 
 # ARCHITECTURE LIST
-ARCH=(
+ARCHS=(
     "linux/amd64"
     "linux/arm/v7"
     "linux/arm64"
 )
 
-# DOCKER LOGIN
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin &> /dev/null
+# CHECK IF DOCKER IS LOGGED
+DOCKER_AUTH_TOKEN=$(cat ~/.docker/config.json | grep \"auth\": | xargs | cut -d ':' -f 2 | xargs)
+if [ -z "$DOCKER_AUTH_TOKEN" ]; then
 
-# BUILD RELEASES
-for RELEASE in "${RELEASES[@]}"; do
-
-    # IMAGE CONFIG AND ARGS
-    GIT_NAME="$(git config --get remote.origin.url | sed 's/.*\/\([^ ]*\/[^.]*\).*/\1/')"
-    GIT_REF="$(git rev-parse --short HEAD)"
-
-    NAME="${GIT_NAME:-ALT_NAME}"
-    VERSION="$RELEASE"
-    BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
-    VCS_REF="${GIT_REF:-ALT_REF}"
-    PLATFORMS="$(echo ${ARCH[@]} | sed 's/ /,/g')"
-
-    if [ "$RELEASE" = "$LATEST" ]; then
-        docker buildx build \
-            --push \
-            --progress auto \
-            --build-arg VERSION="$VERSION" \
-            --build-arg VCS_REF="$VCS_REF" \
-            --build-arg BUILD_DATE="$BUILD_DATE" \
-            --platform "$PLATFORMS" \
-            --tag "$NAME:$RELEASE" \
-            --tag "$NAME:latest" \
-            --cache-from "$NAME:latest" \
-            .
+    # NOT LOGGED IN
+    # Check if $DOCKER_USER is empty
+    if [ -z "$DOCKER_USER" ]; then
+        # login via command line
+        docker login
     else
-        docker buildx build \
-            --push \
-            --progress auto \
-            --build-arg VERSION="$VERSION" \
-            --build-arg VCS_REF="$VCS_REF" \
-            --build-arg BUILD_DATE="$BUILD_DATE" \
-            --platform "$PLATFORMS" \
-            --tag "$NAME:$RELEASE" \
-            --cache-from "$NAME:$RELEASE" \
-            .
+        # login via command line using --password-stdin
+        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin &>/dev/null
     fi
 
-done
+else
+    # LOGGED
+    echo "Docker appears to be logged in, step skipped."
+fi
 
+# ========================= BUILD =========================
+# PRINT DOWNLOAD docker-entrypoint.sh AND prepare-run-env.sh
+echo "DOWNLOAD docker-entrypoint.sh AND prepare-run-env.sh"
+
+# DOWNLOAD docker-entrypoint.sh AND prepare-run-env.sh
+wget -q -O docker-entrypoint.sh -c https://raw.githubusercontent.com/mysteriumnetwork/node/master/bin/docker/docker-entrypoint.sh
+wget -q -O prepare-run-env.sh -c https://raw.githubusercontent.com/mysteriumnetwork/node/master/bin/helpers/prepare-run-env.sh
+chmod +x docker-entrypoint.sh prepare-run-env.sh
+
+# SEARCH RELEASES FOR BUILD
+for RELEASE in "${RELEASES[@]}"; do
+
+    # PRINT BUILD INFO
+    echo " ========= BUILDING RELEASE: $RELEASE ========= "
+
+    # Download myst-node .DEB PACKAGE
+    for ARCH in "${ARCHS[@]}"; do
+
+        # PRINT DOWNLOAD INFO
+        echo "DOWNLOAD .DEB PACKAGE $RELEASE FOR $ARCH"
+
+        # CHECK ARCH
+        case "$ARCH" in
+        linux/386) _ARCH="" ;;
+        linux/amd64) _ARCH="amd64" ;;
+        linux/arm/v6) _ARCH="armhf" ;;
+        linux/arm/v7) _ARCH="armhf" ;;
+        linux/arm64) _ARCH="arm64" ;;
+        linux/ppc64le) _ARCH="" ;;
+        esac
+
+        # DOWNLOAD MYST-NODE .DEB PACKAGE
+        mkdir -p "$ARCH"
+        wget -q -O $ARCH/myst_node.deb -c https://github.com/mysteriumnetwork/node/releases/download/$RELEASE/myst_linux_$_ARCH.deb
+    done
+
+    # PRINT BUILD INFO
+    echo "STARTING THE BUILD"
+
+    # Build using BUILDX
+    # ADD TAG LATEST IF $RELEASE = $LATEST_RELEASE
+    if [ "$RELEASE" = "$LATEST_RELEASE" ]; then
+        docker buildx build \
+        --push \
+        --build-arg IMAGE_NAME="$IMAGE_NAME" \
+        --build-arg IMAGE_VER="${IMAGE_VER:-RELEASE}" \
+        --build-arg IMAGE_AUTHOR="$IMAGE_AUTHOR" \
+        --build-arg IMAGE_VENDOR="$IMAGE_VENDOR" \
+        --build-arg IMAGE_REF="${IMAGE_REF:-IMAGE_ALT_REF}" \
+        --build-arg IMAGE_DESC="$IMAGE_DESC" \
+        --build-arg IMAGE_BUILD_DATE="$IMAGE_BUILD_DATE" \
+        --build-arg IMAGE_URL="$IMAGE_URL" \
+        --build-arg IMAGE_LICENSE="$IMAGE_LICENSE" \
+        --cache-from "${IMAGE_NAME}:latest" \
+        --platform "$(echo ${ARCHS[@]} | sed 's/ /,/g')" \
+        -t "${IMAGE_NAME}:${IMAGE_VER:-RELEASE}" \
+        -t "${IMAGE_NAME}:latest" \
+        .
+    else
+        docker buildx build \
+        --push \
+        --build-arg IMAGE_NAME="$IMAGE_NAME" \
+        --build-arg IMAGE_VER="${IMAGE_VER:-RELEASE}" \
+        --build-arg IMAGE_AUTHOR="$IMAGE_AUTHOR" \
+        --build-arg IMAGE_VENDOR="$IMAGE_VENDOR" \
+        --build-arg IMAGE_REF="${IMAGE_REF:-IMAGE_ALT_REF}" \
+        --build-arg IMAGE_DESC="$IMAGE_DESC" \
+        --build-arg IMAGE_BUILD_DATE="$IMAGE_BUILD_DATE" \
+        --build-arg IMAGE_URL="$IMAGE_URL" \
+        --build-arg IMAGE_LICENSE="$IMAGE_LICENSE" \
+        --cache-from "${IMAGE_NAME}:latest" \
+        --platform "$(echo ${ARCHS[@]} | sed 's/ /,/g')" \
+        -t "${IMAGE_NAME}:${IMAGE_VER:-RELEASE}" \
+        .
+    fi
+
+    # PRINT DEL INFO
+    echo "Removing files used in construction"
+
+    # Remove Files
+    rm -rf linux docker-entrypoint.sh prepare-run-env.sh
+
+    # PRINT BUILD INFO
+    echo " ========= Construction completed successfully ========= "
+done
