6cdb5bbd2a236fb3c40b30fafeeb881a6b5b5674
Pedro Filipe <xpecex@outlook.com>
2021-08-11 01:26:25 -0300

    add 0.53.1 version

--- build.sh
+++ build.sh
@@ -24,6 +24,7 @@ RELEASES=(
     "0.51.0"
     "0.52.0"
     "0.53.0"
+    "0.53.1"
 )
 # LATEST RELEASE
 LATEST_RELEASE=$(curl -s "https://github.com/mysteriumnetwork/node/releases/latest" | cut -d'/' -f 8 | cut -d'"' -f 1)
